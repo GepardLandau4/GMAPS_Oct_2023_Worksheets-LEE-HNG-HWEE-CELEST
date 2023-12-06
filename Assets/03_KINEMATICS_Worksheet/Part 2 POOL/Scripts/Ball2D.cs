c05d090b6150cd2d918ace5e00c0e984ac5d2b6b
GepardLandau4 <2201733I@student.tp.edu.sg>
2023-12-06 20:44:48 +0800

    Kinematics Part 2 Pool

--- Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Ball2D.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Ball2D.cs
@@ -1,53 +1,58 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Ball2D : MonoBehaviour
-// {
-//     public HVector2D Position = new HVector2D(0, 0);
-//     public HVector2D Velocity = new HVector2D(0, 0);
+public class Ball2D : MonoBehaviour
+{
+    public HVector2D Position = new HVector2D(0, 0);
+    public HVector2D Velocity = new HVector2D(0, 0);
     
-//     [HideInInspector]
-//     public float Radius;
+    [HideInInspector]
+    public float Radius;
 
-//     private void Start()
-//     {
-//         Position.x = transform.position.x;
-//         Position.y = transform.position.y;
+    private void Start()
+    {
+        Position.x = transform.position.x;
+        Position.y = transform.position.y;
 
-//         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-//         Vector2 sprite_size = sprite.rect.size;
-//         Vector2 local_sprite_size = sprite_size / sprite.pixelsPerUnit;
-//         Radius = local_sprite_size.x / 2f;
-//     }
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Vector2 sprite_size = sprite.rect.size;
+        Vector2 local_sprite_size = sprite_size / sprite.pixelsPerUnit;
+        Radius = local_sprite_size.x / 2f;
 
-//     public bool IsCollidingWith(float x, float y)
-//     {
-//         float distance = /*your code here*/;
-//         return distance <= Radius;
-//     }
+        // HVector2D a = new HVector2D(8f, 5f);
+        // HVector2D b = new HVector2D(1f, 3f);
+        // float distance = Util.FindDistance(a,b);
+        // Debug.Log(distance);
+    }
 
-//     public bool IsCollidingWith(Ball2D other)
-//     {
-//         float distance = Util.FindDistance(Position, other.Position);
-//         return distance <= Radius + other.Radius;
-//     }
+    public bool IsCollidingWith(float x, float y)
+    {
+        float distance = Mathf.Sqrt(Mathf.Pow(Position.x - x, 2f) + Mathf.Pow(Position.y - y, 2f));
+        return distance <= Radius;
+    }
 
-//     public void FixedUpdate()
-//     {
-//         UpdateBall2DPhysics(Time.deltaTime);
-//     }
+    // public bool IsCollidingWith(Ball2D other)
+    // {
+    //     float distance = Util.FindDistance(Position, other.Position);
+    //     return distance <= Radius + other.Radius;
+    // }
 
-//     private void UpdateBall2DPhysics(float deltaTime)
-//     {
-//         float displacementX = /*your code here*/;
-//         float displacementY = /*your code here*/;
+    public void FixedUpdate()
+    {
+        UpdateBall2DPhysics(Time.deltaTime);
+    }
 
-//         Position.x += /*your code here*/;
-//         Position.y += /*your code here*/;
+    private void UpdateBall2DPhysics(float deltaTime)
+    {
+        float displacementX = Velocity.x * deltaTime;
+        float displacementY = Velocity.y * deltaTime;
 
-//         transform.position = new Vector2(/*your code here*/);
-//     }
-// }
+        Position.x += displacementX;
+        Position.y += displacementY;
+
+        transform.position = new Vector2(Position.x, Position.y);
+    }
+}
 
