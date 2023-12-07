6f4262e44b0f68df7878240860ae8cc76d025603
GepardLandau4 <2201733I@student.tp.edu.sg>
2023-12-07 14:38:32 +0800

    Matrices Comment 2

--- Assets/02_MATRICES_worksheet/Sonic/HMatrix2D.cs
+++ Assets/02_MATRICES_worksheet/Sonic/HMatrix2D.cs
@@ -134,23 +134,24 @@ public class HMatrix2D
         return mulVec;
     }
     
-    //This function returns the product of two matrices by 
+    //This operator returns the product of two matrices by setting each element in the matrix to the dot product of the xth row of the 
+    //left matrix and the yth column of the right matrix.
     public static HMatrix2D operator *(HMatrix2D left, HMatrix2D right)
     {
-        //Create a new HMatrix2D to store the result of the matrix multiplication
+        //Declare a HVector2D called mulVec that stores result of the multiplication of the two HMatrix2Ds 
         HMatrix2D mulMat2 = new HMatrix2D();
 
-        //This will loop through the next lines of code three times, with y representing the column number of the new matrix/column 
-        //number of the right matrix
+        //This will loop through the next lines of code for each column in the resulting matrix, with y representing the column number 
+        //of the new matrix/column number of the right matrix
         for (int y = 0; y < 3; y++)
         {
-            //This will loop through the next lines of code three times, with x representing the row number of the new matrix/row number
-            //of the left matrix
+            //This will loop through the next lines of code for each row in the resulting matrix, with x representing the row number 
+            //of the new matrix/row number of the left matrix
             for (int x = 0; x < 3; x++)
             {
-                //This will loop throuh the next lines of code 3 times, with i representing the column number of the left matrix/row
-                //number of the right matrix. This needs to be done as each value in the new matrix requires 3 different values to be 
-                //calculated by multiplying a value from each of the matrices and added together
+                //This will set the xth row and the yth column of mulMat2 to the dot product of the corresponding row of left and
+                //columns of right by adding each multiplication of the corresponding elements to the xth row and the yth column of 
+                //mulMat2, with i representing the ith element of left's row x and the ith element of right's column y
                 for (int i = 0; i < 3; i++)
                 {
                     //Add the product of row x column i of the left matrix and row i column y of the right matrix to row x column y of
@@ -165,39 +166,57 @@ public class HMatrix2D
  
     } 
     
+    //This operator is used to check if the left and right matrices are equal (same corresponding elements) and will return true if so
     public static bool operator ==(HMatrix2D left, HMatrix2D right)
     {
+        //For each column (y) in the matrices
         for (int y = 0; y < 3; y++)
         {
+            //For each row (x) in the matrices
             for (int x = 0; x < 3; x++)
             {
-                if (left.entries[y,x] != right.entries[y,x])
+                //If any of the elements of the same row and column from the left and right matrices are not equal, stop the loops and
+                //immediately return false to indicate that the matrices are not equal
+                if (left.entries[x,y] != right.entries[x,y])
                 {
                     return false;
                 }
             }
         }
+        //If all the loops have been gone through without getting interrupted, it means that none of the corresponding elements in the
+        //two matrices are not equal, thus all the elements in the two matrices are equal, which means the matrices are equal, thus
+        //true will be returned to indicate the matrices are equal
         return true;
     }
     
-
+    //This operator is used to check if the left and right matrices are not equal (at least one different corresponding element) and 
+    //will return true if so
+    //This operator works similarly to the == operator, but with the true and false swapped
     public static bool operator !=(HMatrix2D left, HMatrix2D right)
     {
+        //For each column (y) in the matrices
         for (int y = 0; y < 3; y++)
         {
+            //For each row (x) in the matrices
             for (int x = 0; x < 3; x++)
             {
-                if (left.entries[y,x] != right.entries[y,x])
+                //If any of the elements of the same row and column from the left and right matrices are not equal, stop the loops and
+                //immediately return true to indicate that the matrices are not equal
+                if (left.entries[x,y] != right.entries[x,y])
                 {
                     return true;
                 }
             }
         }
+        //If all the loops have been gone through without getting interrupted, it means that none of the corresponding elements in the
+        //two matrices are not equal, thus all the elements in the two matrices are equal, which means the matrices are equal, thus
+        //flase will be returned to indicate the matrices are equal
         return false;
     }
 
-    /*
 
+    //Honestly, i do not know but those functions are for
+    /*
     public override bool Equals(object obj)
     {
         // your code here
@@ -219,10 +238,15 @@ public class HMatrix2D
     }
     */
 
+    //This function turn turns the HMatrix2D into an identity HMatrix2D by looping through each element in the HMatrix2D and setting the
+    //values to 0 and 1 if the row number is equal to the column number as identity matrices are matrices where the elements on the main
+    //diagonal are equal to 1 (row number = column number) while the rest of the elements are equal to 0
     public void setIdentity()
     {
+        //For each column (y) in the matrix
         for (int y = 0; y < 3; y++)
         {
+            //For each row (x) in the matrix
             for (int x = 0; x < 3; x++)
             {
                 /*
@@ -235,47 +259,72 @@ public class HMatrix2D
                     entries[y,x] = 0;
                 }
                 */
-                entries[y,x] = (x == y) ? 1 : 0;
+                //If x == y returns true (the row number equals the column number), set the element in row x and column y to 1, however 
+                //if it returns false (the row number does not equal the column number), set the element in row x and column y to 0
+                entries[x,y] = (x == y) ? 1 : 0;
             }
         }
         
     }
 
+    //This function takes in the x and y translation values to turn a HMatrix2D into a translation matrix
     public void setTranslationMat(float transX, float transY)
     {
+        //Set the HMatrix2D to an identity matrix (i assume its to reset the matrix?)
         setIdentity();
+        //Set the third element in rows 1 and 2 to the appropriate translation values
         entries[0,2] = transX;
         entries[1,2] = transY;
     }
 
+    //This function takes in the angle of rotation (in degrees) to turn a HMatrix2D into a rotation matrix
     public void setRotationMat(float rotDeg)
     {
+        //Set the HMatrix2D to an identity matrix (i assume its to reset the matrix?)
         setIdentity();
+        //Convert the rotDeg (which is in degrees) into radians by multiplying it by pi/180 and storing it in rad. This has to be done
+        //Mathf.Cos and Mathf.Sin requires the angle to be in radians in order to calculate the values
         float rad = rotDeg * (Mathf.PI / 180);
+        //Set the elements that are in both the first 2 rows and columns to the appropriate rotation values as 
+        //new x position = old x * cos(angle of rotation) - old y * sin(angle of rotation) and
+        //new y position = old x * sin(angle of rotation) + old y * cos(angle of rotation)
         entries[0,0] = Mathf.Cos(rad);
         entries[0,1] = -Mathf.Sin(rad);
         entries[1,0] = Mathf.Sin(rad);
         entries[1,1] = Mathf.Cos(rad);
     }
 
+    //This function takes in the x and y scale values to turn a HMatrix2D into a scaling matrix
     public void setScalingMat(float scaleX, float scaleY)
     {
+        //Set the HMatrix2D to an identity matrix (i assume its to reset the matrix?)
         setIdentity();
+
+        //Set the first element in the first row and the second element in the second row to the appropriate scaling values
         entries[0,0] = scaleX;
         entries[1,1] = scaleY;
     }
 
+    //This function prints the matrix with Debug.Log so that it can be easily viewed in the console
     public void Print()
     {
+        //Declare the print result as a string
         string result = "";
+
+        //For each row (r) in the matrix, print out a while row
         for (int r = 0; r < 3; r++)
         {
+            //For each column (c) in the matrix, print out each value in the row
             for (int c = 0; c < 3; c++)
             {
+                //Add the value from the rth row and the cth column to the result, then add a space so that each value will be separated
+                //and be easier to view
                 result += entries[r, c] + "  ";
             }
+            //After printing out a whole row, make sure the next row will be printed out on a new line so that it is easier to view
             result += "\n";
         }
+        //Display the result string in the console
         Debug.Log(result);
     }
 }
--- Assets/02_MATRICES_worksheet/Sonic/TestMatrix.cs
+++ Assets/02_MATRICES_worksheet/Sonic/TestMatrix.cs
@@ -11,7 +11,7 @@ public class TestMatrix : MonoBehaviour
 
    void Start()
    {
-        
+
    }
 
 }
